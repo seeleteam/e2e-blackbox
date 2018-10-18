2836472ade6dc4fbeafefc08a08563809d0c63bd
yanllearnn <yanllearnn@163.com>
2018-10-18 14:22:33 +0800

    add test case of htlc and others

--- testcase/light_test.go
+++ testcase/light_test.go
@@ -8,157 +8,13 @@ package testcase
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"os/exec"
-	"strconv"
 	"strings"
 	"testing"
 	"time"
 )
-
-type BalanceInfo struct {
-	Account string
-	Balance int64
-}
-
-type BlockInfo struct {
-	Hash         string        `json:"hash"`
-	Transactions []interface{} `json:"transactions"`
-}
-
-type TxDataInfo struct {
-	From   string
-	To     string
-	Amount int
-}
-
-type TxInfo struct {
-	Hash   string     `json:"hash"`
-	TxData TxDataInfo `json:"Data"`
-}
-
-type ReceiptInfo struct {
-	Failed   bool `json:"failed"`
-	TotalFee int  `json:"totalFee"`
-	UsedGas  int  `json:"usedGas"`
-}
-
-type PoolTxInfo struct {
-	Hash   string `json:"hash"`
-	Nonce  int    `json:"accountNonce"`
-	Amount int64  `json:"amount"`
-	//From     string `json:"from"`
-	//To       string `json:"to"`
-	//GasLimit int    `json:"gasLimit"`
-}
-
-type SendTxInfo struct {
-	nonce   int
-	hash    string
-	amount  int64
-	gasUsed int
-	bMined  bool
-}
-
-func accountCase(command, account, accountMix string, t *testing.T) {
-	cmd := exec.Command(CmdLight, "getbalance", "--account", account, "--address", ServerAddr)
-	var output, outputMix []byte
-	var err error
-	if output, err = cmd.CombinedOutput(); err != nil {
-		t.Fatalf("getbalance err: %s", err)
-	}
-
-	cmd = exec.Command(CmdLight, "getbalance", "--account", accountMix, "--address", ServerAddr)
-	if outputMix, err = cmd.CombinedOutput(); err != nil {
-		t.Fatalf("getbalance err: %s", err)
-	}
-
-	if string(output) != string(outputMix) {
-		t.Fail()
-	}
-}
-
-func getBalance(t *testing.T, command, account, serverAddr string) (int64, error) {
-	cmd := exec.Command(command, "getbalance", "--account", account, "--address", serverAddr)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		return 0, err
-	}
-
-	var info BalanceInfo
-	if err = json.Unmarshal(output, &info); err != nil {
-		return 0, err
-	}
-
-	return info.Balance, nil
-}
-
-func getNonce(t *testing.T, command, account, serverAddr string) (int, error) {
-	cmd := exec.Command(command, "getnonce", "--account", account, "--address", serverAddr)
-	//var curNonce int
-	if output, err := cmd.CombinedOutput(); err != nil {
-		return 0, err
-	} else {
-		output = bytes.Trim(output, "\n")
-		fmt.Println(string(output))
-		return strconv.Atoi(string(output))
-	}
-}
-
-func sentTX(t *testing.T, command string, ammount, nonce int, account1_keystore, account2, serverAddr string) (txHash, debtHash string, err error) {
-	cmd := exec.Command(command, "sendtx", "--amount", strconv.Itoa(ammount), "--price", "1", "--from", account1_keystore, "--to", account2, "--nonce", strconv.Itoa(nonce))
-	stdin, err := cmd.StdinPipe()
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer stdin.Close()
-
-	var out bytes.Buffer
-	var outErr bytes.Buffer
-	cmd.Stdout, cmd.Stderr = &out, &outErr
-
-	if err = cmd.Start(); err != nil {
-		return
-	}
-
-	io.WriteString(stdin, "123456\n")
-	cmd.Wait()
-
-	outStr, errStr := out.String(), outErr.String()
-	if len(string(errStr)) > 0 {
-		err = errors.New(string(errStr))
-		return
-	}
-
-	outStr = outStr[strings.Index(outStr, "{"):]
-	outStr = strings.Trim(outStr, "\n")
-	outStr = strings.Trim(outStr, " ")
-	fmt.Println("sendtx out:[", outStr, "]")
-	var txInfo TxInfo
-	if err = json.Unmarshal([]byte(outStr), &txInfo); err != nil {
-		return
-	}
-
-	txHash = txInfo.Hash
-	return
-}
-
-func getReceipt(t *testing.T, command, txHash, serverAddr string) (info *ReceiptInfo, err error) {
-	var output []byte
-	cmd := exec.Command(command, "getreceipt", "--hash", txHash, "--address", serverAddr)
-	if output, err = cmd.CombinedOutput(); err != nil {
-		fmt.Println("exec err:", err, cmd.Args)
-	} else {
-		fmt.Println("receipt:", string(output))
-		var receiptInfo ReceiptInfo
-		if err = json.Unmarshal(output, &receiptInfo); err == nil {
-			info = &receiptInfo
-		}
-	}
-	return
-}
 
 /*
 func getPendingTxs(t *testing.T, command, serverAddr string) (infoL []*PoolTxInfo, err error) {
@@ -177,60 +33,6 @@ func getPendingTxs(t *testing.T, command, serverAddr string) (infoL []*PoolTxInf
 	return
 }
 */
-func getPendingTxs(t *testing.T, command, serverAddr string) (infoL []PoolTxInfo, err error) {
-	var output []byte
-	cmd := exec.Command(command, "getpendingtxs", "--address", serverAddr)
-	if output, err = cmd.CombinedOutput(); err != nil {
-		return
-	}
-	//fmt.Println("pendingtxs:", string(output))
-	var curL []PoolTxInfo
-	if err = json.Unmarshal(output, &curL); err == nil {
-		fmt.Println("getPendingTx:", curL)
-		infoL = curL
-	}
-	return
-}
-
-func getPoolContentTxs(t *testing.T, command, serverAddr string) (infoM map[string][]PoolTxInfo, err error) {
-	var output []byte
-	cmd := exec.Command(command, "gettxpoolcontent", "--address", serverAddr)
-	if output, err = cmd.CombinedOutput(); err != nil {
-		return
-	}
-	//fmt.Println("gettxpoolcontent:", string(output))
-	//var curM map[string][]PoolTxInfo
-	if err = json.Unmarshal(output, &infoM); err == nil {
-		/*for key, item := range curM {
-			var infoL []*PoolTxInfo
-			//infoL = append(infoL, &item)
-			fmt.Println(key,item)
-		}*/
-	}
-	return
-}
-
-func findTxHashFromPool(txHash string, infoL *[]PoolTxInfo, infoM *map[string][]PoolTxInfo) (bPending, bContentPool bool) {
-	if infoL != nil {
-		for _, info := range *infoL {
-			if info.Hash == txHash {
-				bPending = true
-			}
-		}
-	}
-
-	if infoM != nil {
-		for _, itemL := range *infoM {
-			for _, info := range itemL {
-				if info.Hash == txHash {
-					bContentPool = true
-				}
-			}
-		}
-	}
-
-	return
-}
 
 // account should ignore character case.
 func Test_Light_AccountIgnoreCase(t *testing.T) {
@@ -436,18 +238,18 @@ func Test_Light_SendTx_RemoveTimestamp(t *testing.T) {
 func Test_Light_GetReceipt(t *testing.T) {
 	curNonce, err := getNonce(t, CmdLight, Account1, ServerAddr)
 	if err != nil {
-		t.Fatalf("getnonce returns with error input", err)
+		t.Fatalf("getnonce returns with error input err: %s", err)
 	}
 
 	var beginBalance, dstBeginBalance int64
 	beginBalance, err = getBalance(t, CmdLight, Account1, ServerAddr)
 	if err != nil {
-		t.Fatalf("getBalance returns with error input", err)
+		t.Fatalf("getBalance returns with error input err: %s", err)
 	}
 
 	dstBeginBalance, err = getBalance(t, CmdLight, Account1_Aux, ServerAddr)
 	if err != nil {
-		t.Fatalf("getBalance returns with error input", err)
+		t.Fatalf("getBalance returns with error input err: %s", err)
 	}
 
 	fmt.Println("account1=", beginBalance, "dstAccount=", dstBeginBalance)
@@ -456,7 +258,7 @@ func Test_Light_GetReceipt(t *testing.T) {
 
 	for cnt := 0; cnt < 100; cnt++ {
 		itemNonce := curNonce + 2 + cnt
-		txHash, _, err = sentTX(t, CmdLight, 10000, itemNonce, "./shard1-0xa00d22dc3624d4696eff8d1641b442f79c3379b1.keystore", Account1_Aux, ServerAddr)
+		txHash, _, err = sentTX(t, CmdLight, 10000, itemNonce, KeyFileShard1_1, AccountShard1_2, ServerAddr)
 		if err != nil {
 			t.Fatalf("Test_Light_SendTx: An error occured: %s", err)
 		}
@@ -513,12 +315,12 @@ func Test_Light_GetReceipt(t *testing.T) {
 	var endBalance, dstEndBalance int64
 	endBalance, err = getBalance(t, CmdLight, Account1, ServerAddr)
 	if err != nil {
-		t.Fatalf("getBalance returns with error input", err)
+		t.Fatalf("getBalance returns with error input err: %s", err)
 	}
 
 	dstEndBalance, err = getBalance(t, CmdLight, Account1_Aux, ServerAddr)
 	if err != nil {
-		t.Fatalf("getBalance returns with error input", err)
+		t.Fatalf("getBalance returns with error input err: %s", err)
 	}
 
 	fmt.Println("account1=", endBalance, "dstAccount=", dstEndBalance)
@@ -530,7 +332,7 @@ func Test_Light_GetReceipt(t *testing.T) {
 }
 
 func Test_Light_SendTx_InvalidAccountLength(t *testing.T) {
-	cmd := exec.Command(CmdLight, "sendtx", "--amount", "10000", "--price", "1", "--from", "./shard1-0xa00d22dc3624d4696eff8d1641b442f79c3379b1.keystore", "--to", "0x")
+	cmd := exec.Command(CmdLight, "sendtx", "--amount", "10000", "--price", "1", "--from", KeyFileShard1_1, "--to", "0x")
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
 		fmt.Println(err)
@@ -556,7 +358,7 @@ func Test_Light_SendTx_InvalidAccountLength(t *testing.T) {
 }
 
 func Test_Light_SendTx_InvalidAccountType(t *testing.T) {
-	cmd := exec.Command(CmdLight, "sendtx", "--amount", "10000", "--price", "1", "--from", "./shard2-0xc910e52e3a314c93fdf545b88d264f39becb8d41.keystore", "--to", InvalidAccountType)
+	cmd := exec.Command(CmdLight, "sendtx", "--amount", "10000", "--price", "1", "--from", KeyFileShard2_1, "--to", InvalidAccountType)
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
 		fmt.Println(err)
@@ -613,7 +415,7 @@ func Test_Light_GetShardNum(t *testing.T) {
 func Test_Light_GetNonce_InvalidAccount0x(t *testing.T) {
 	cmd := exec.Command(CmdLight, "getnonce", "--account", "0x", "--address", ServerAddr)
 	if _, err := cmd.CombinedOutput(); err == nil {
-		t.Fatalf("getnonce returns with error input", err)
+		t.Fatalf("getnonce returns with error input err: %s", err)
 	}
 }
 
@@ -625,7 +427,7 @@ func Test_Light_GetNonce_InvalidAccount(t *testing.T) {
 }
 
 func Test_Light_GetNonce_AccountFromOtherShard(t *testing.T) {
-	cmd := exec.Command(CmdLight, "getnonce", "--account", Account1, "--address", ServerAddr)
+	cmd := exec.Command(CmdLight, "getnonce", "--account", AccountShard1_1, "--address", ServerAddr)
 	if _, err := cmd.CombinedOutput(); err == nil {
 		t.Fatalf("getnonce returns successfully for other shard account")
 	}
