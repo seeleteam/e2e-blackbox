75bf631c393b30a5a6361372da14a362298575b1
forjoin92 <forjoin92@gmail.com>
2018-11-01 17:28:09 +0800

    add Seele Client Commands
    1.  add P2P commands
    2. add miner commands
    3. add dumpheap command
    4. add payload command

--- testcase/network_test.go
+++ testcase/network_test.go
@@ -6,8 +6,10 @@
 package testcase
 
 import (
+	"bytes"
 	"encoding/json"
 	"os/exec"
+	"strconv"
 	"testing"
 )
 
@@ -25,41 +27,97 @@ type PeerInfo struct {
 func Test_Client_P2P_NetVersion(t *testing.T) {
 	cmd := exec.Command(CmdClient, "p2p", "netversion", "--address", ServerAddr)
 	if _, err := cmd.CombinedOutput(); err != nil {
-		t.Fatalf("%s", err)
+		t.Fatalf("Test_Client_P2P_NetVersion: An error occured: %s", err.Error())
 	}
 }
 
 func Test_Client_P2P_NetworkID(t *testing.T) {
 	cmd := exec.Command(CmdClient, "p2p", "networkid", "--address", ServerAddr)
 	if _, err := cmd.CombinedOutput(); err != nil {
-		t.Fatalf("%s", err)
+		t.Fatalf("Test_Client_P2P_NetworkID: An error occured: %s", err.Error())
 	}
 }
 
 func Test_Client_P2P_Peers(t *testing.T) {
 	cmd := exec.Command(CmdClient, "p2p", "peers", "--address", ServerAddr)
-	if _, err := cmd.CombinedOutput(); err != nil {
-		t.Fatalf("%s", err)
+	peers, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Test_Client_P2P_Peers: An error occured: %s", err.Error())
+	}
+	peers = bytes.TrimRight(peers, "\n")
+	n, err := strconv.ParseInt(string(peers), 10, 64)
+	if err != nil {
+		t.Fatalf("parse string to int64 failed %s", err.Error())
+	}
+	if n < 0 {
+		t.Fatalf("Test_Client_P2P_Peers returns invalid peer number")
 	}
 }
 
-func Test_Client_P2P_ProtocolVersiont(t *testing.T) {
+func Test_Client_P2P_ProtocolVersion(t *testing.T) {
 	cmd := exec.Command(CmdClient, "p2p", "protocolversion", "--address", ServerAddr)
 	if _, err := cmd.CombinedOutput(); err != nil {
-		t.Fatalf("%s", err)
+		t.Fatalf("Test_Client_P2P_ProtocolVersion: An error occured: %s", err.Error())
 	}
 }
 
 func Test_Client_P2P_PeersInfo(t *testing.T) {
 	cmd := exec.Command(CmdClient, "p2p", "peersinfo", "--address", ServerAddr)
 	if output, err := cmd.CombinedOutput(); err != nil {
-		t.Fatalf("%s", err)
+		t.Fatalf("Test_Client_P2P_PeersInfo: An error occured: %s", err.Error())
 	} else {
 		var peersInfo []PeerInfo
 		if err = json.Unmarshal(output, &peersInfo); err != nil {
 			t.Fatalf("%s", err)
 		}
-		//fmt.Println("peersinfo:", string(output))
-		//fmt.Println("infoL:", peersInfo)
+	}
+}
+
+func Test_Light_P2P_NetVersion(t *testing.T) {
+	cmd := exec.Command(CmdLight, "p2p", "netversion", "--address", ServerAddr)
+	if _, err := cmd.CombinedOutput(); err != nil {
+		t.Fatalf("Test_Light_P2P_NetVersion: An error occured: %s", err.Error())
+	}
+}
+
+func Test_Light_P2P_NetworkID(t *testing.T) {
+	cmd := exec.Command(CmdLight, "p2p", "networkid", "--address", ServerAddr)
+	if _, err := cmd.CombinedOutput(); err != nil {
+		t.Fatalf("Test_Light_P2P_NetworkID: An error occured: %s", err.Error())
+	}
+}
+
+func Test_Light_P2P_Peers(t *testing.T) {
+	cmd := exec.Command(CmdLight, "p2p", "peers", "--address", ServerAddr)
+	peers, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Test_Client_P2P_Peers: An error occured: %s", err.Error())
+	}
+	peers = bytes.TrimRight(peers, "\n")
+	n, err := strconv.ParseInt(string(peers), 10, 64)
+	if err != nil {
+		t.Fatalf("parse string to int64 failed %s", err.Error())
+	}
+	if n < 0 {
+		t.Fatalf("Test_Client_P2P_Peers returns invalid peer number")
+	}
+}
+
+func Test_Light_P2P_ProtocolVersion(t *testing.T) {
+	cmd := exec.Command(CmdLight, "p2p", "protocolversion", "--address", ServerAddr)
+	if _, err := cmd.CombinedOutput(); err != nil {
+		t.Fatalf("Test_Light_P2P_ProtocolVersion: An error occured: %s", err.Error())
+	}
+}
+
+func Test_Light_P2P_PeersInfo(t *testing.T) {
+	cmd := exec.Command(CmdLight, "p2p", "peersinfo", "--address", ServerAddr)
+	if output, err := cmd.CombinedOutput(); err != nil {
+		t.Fatalf("Test_Light_P2P_PeersInfo: An error occured: %s", err.Error())
+	} else {
+		var peersInfo []PeerInfo
+		if err = json.Unmarshal(output, &peersInfo); err != nil {
+			t.Fatalf("%s", err)
+		}
 	}
 }
