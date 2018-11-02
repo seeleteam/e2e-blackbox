3ee5003a506ae40d9c4cd2f3812807730f68c729
lizhengwei88 <408212402@qq.com>
2018-11-02 11:33:59 +0800

    getblock

--- testcase/client_test.go
+++ testcase/client_test.go
@@ -12,6 +12,7 @@ import (
 	"regexp"
 	"strings"
 	"testing"
+	"time"
 )
 
 type ResGetInfo struct {
@@ -80,9 +81,24 @@ func Test_Client_DumpHeap(t *testing.T) {
 
 func Test_Client_GetBlock_ByNormalHeight(t *testing.T) {
 	// Normal height
+ErrContinue:
 	cmd := exec.Command(CmdClient, "getblock", "--height", "1", "--address", ServerAddr)
-	if _, err := cmd.CombinedOutput(); err != nil {
-		t.Fatalf("getblock: Node not running!")
+	if res, err := cmd.CombinedOutput(); err != nil {
+		t.Logf("Test_Client_GetBlock_ByNormalHeight: Node not running!")
+		time.Sleep(5 * time.Second)
+		goto ErrContinue
+	} else {
+		var blockInfo BlockInfo
+		if err = json.Unmarshal(res, &blockInfo); err != nil {
+			t.Fatalf("Test_Client_GetBlock_ByNormalHeight: %s", err)
+		}
+
+		headerMp := blockInfo.Header.(map[string]interface{})
+		height := uint64(headerMp["Height"].(float64))
+		if height != 1 {
+			t.Fatalf("Test_Client_GetBlock_ByNormalHeight: Expect the return value is not correct!")
+		}
+
 	}
 }
 
@@ -90,14 +106,14 @@ func Test_Client_GetBlock_ByInvalidHeight(t *testing.T) {
 	// invalid height
 	cmd := exec.Command(CmdClient, "getblock", "--height", "10000000000", "--address", ServerAddr)
 	if _, err := cmd.CombinedOutput(); err == nil {
-		t.Fatalf("getblock: error parameter success?")
+		t.Fatalf("Test_Client_GetBlock_ByInvalidHeight: error parameter success?")
 	}
 }
 
 func Test_Client_GetBlock_InvalidParameter(t *testing.T) {
 	cmd := exec.Command(CmdClient, "getblock", "1", "--address", ServerAddr)
 	if _, err := cmd.CombinedOutput(); err != nil {
-		t.Fatalf("getblock error, %s", err)
+		t.Fatalf("Test_Client_GetBlock_InvalidParameter error, %s", err)
 	}
 }
 
@@ -105,7 +121,7 @@ func Test_Client_GetBlock_ByNormalHash(t *testing.T) {
 	// invalid height
 	cmd := exec.Command(CmdClient, "getblock", "--hash", BlockHash, "--address", ServerAddr)
 	if _, err := cmd.CombinedOutput(); err != nil {
-		t.Fatalf("getblock error, %s", err)
+		t.Fatalf("Test_Client_GetBlock_ByNormalHash error, %s", err)
 	}
 }
 
@@ -113,7 +129,7 @@ func Test_Client_GetBlock_ByInvalidHash(t *testing.T) {
 	// invalid height
 	cmd := exec.Command(CmdClient, "getblock", "--hash", BlockHashErr, "--address", ServerAddr)
 	if _, err := cmd.CombinedOutput(); err == nil {
-		t.Fatalf("getblock error parameter success?")
+		t.Fatalf("Test_Client_GetBlock_ByInvalidHash error parameter success?")
 	}
 }
 
@@ -121,13 +137,18 @@ func Test_Client_GetBlock_ByInvalidHash(t *testing.T) {
 func Test_Client_GetBlock_ByHeightFulltx(t *testing.T) {
 	cmd := exec.Command(CmdClient, "getblock", "--height", "1", "--fulltx", "--address", ServerAddr)
 	if output, err := cmd.CombinedOutput(); err != nil {
-		t.Fatalf("getblock error, %s", err)
+		t.Fatalf("Test_Client_GetBlock_ByHeightFulltx error, %s", err)
 	} else {
 		var blockInfo BlockInfo
 		if err = json.Unmarshal(output, &blockInfo); err != nil {
 			t.Fatalf("Test_Client_GetBlock_ByHeightFulltx: %s", err)
 		}
 
+		headerMp := blockInfo.Header.(map[string]interface{})
+		height := uint64(headerMp["Height"].(float64))
+		if height != 1 {
+			t.Fatalf("Test_Client_GetBlock_ByHeightFulltx: Expect the return value is not correct!")
+		}
 		if len(blockInfo.Transactions) <= 0 {
 			t.Fatalf("Test_Client_GetBlock_ByHeightFulltx, block should contain one transaction at lease")
 		}
@@ -138,13 +159,15 @@ func Test_Client_GetBlock_ByHeightFulltx(t *testing.T) {
 func Test_Client_GetBlock_ByHashFulltx(t *testing.T) {
 	cmd := exec.Command(CmdClient, "getblock", "--hash", BlockHash, "--fulltx", "--address", ServerAddr)
 	if output, err := cmd.CombinedOutput(); err != nil {
-		t.Fatalf("getblock error, %s", err)
+		t.Fatalf("Test_Client_GetBlock_ByHashFulltx error, %s", err)
 	} else {
 		var blockInfo BlockInfo
 		if err = json.Unmarshal(output, &blockInfo); err != nil {
 			t.Fatalf("Test_Client_GetBlock_ByHashFulltx: %s", err)
 		}
-
+		if blockInfo.Hash != "0x000000c80d93ee588e022dfa396357c6f1f77b4f8576d8188dcbb821ed742900" {
+			t.Fatalf("Test_Client_GetBlock_ByHashFulltx: Expect the return value is not correct!")
+		}
 		if len(blockInfo.Transactions) <= 0 {
 			t.Fatalf("Test_Client_GetBlock_ByHashFulltx, hash should contain one transaction at lease")
 		}
