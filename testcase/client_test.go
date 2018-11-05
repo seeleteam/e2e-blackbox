d57d07def3bdbc3891150accd6356d327a2c90e3
lizhengwei88 <408212402@qq.com>
2018-11-05 15:03:39 +0800

    getblock1105

--- testcase/client_test.go
+++ testcase/client_test.go
@@ -552,3 +552,103 @@ func Test_Client_SaveKey(t *testing.T) {
 }
 
 // --------------------test savekey end-------------------
+
+func Test_Client_GetBlock_ByHeight_NodeStop(t *testing.T) {
+	// Normal height
+	cmd := exec.Command(CmdClient, "getblock", "--height", "1", "--address", ServerAddr)
+	if _, err := cmd.CombinedOutput(); err != nil {
+		t.Fatalf("Test_Client_GetBlock_ByHeight_NodeStop: Node not running!")
+	}
+}
+
+func Test_Client_GetBlock_ByHeight_NodeStart(t *testing.T) {
+	// Normal height
+	cmd := exec.Command(CmdClient, "getblock", "--height", "1", "--address", ServerAddr)
+	if res, err := cmd.CombinedOutput(); err != nil {
+		t.Fatalf("Test_Client_GetBlock_ByHeight_NodeStart:Node to run returns error: %s", err)
+	} else {
+		var blockInfo BlockInfo
+		if err = json.Unmarshal(res, &blockInfo); err != nil {
+			t.Fatalf("Test_Client_GetBlock_ByHeight_NodeStart: %s", err)
+		}
+
+		headerMp := blockInfo.Header.(map[string]interface{})
+		height := uint64(headerMp["Height"].(float64))
+		if height != 1 {
+			t.Fatalf("Test_Client_GetBlock_ByHeight_NodeStart: Expect the return value is not correct!")
+		}
+
+	}
+}
+
+func Test_Client_GetBlock_ByInvalidHeight(t *testing.T) {
+	// invalid height
+	cmd := exec.Command(CmdClient, "getblock", "--height", "10000000000", "--address", ServerAddr)
+	if _, err := cmd.CombinedOutput(); err == nil {
+		t.Fatalf("Test_Client_GetBlock_ByInvalidHeight: error parameter success?")
+	}
+}
+
+func Test_Client_GetBlock_InvalidParameter(t *testing.T) {
+	cmd := exec.Command(CmdClient, "getblock", "1", "--address", ServerAddr)
+	if _, err := cmd.CombinedOutput(); err != nil {
+		t.Fatalf("Test_Client_GetBlock_InvalidParameter error, %s", err)
+	}
+}
+
+func Test_Client_GetBlock_ByNormalHash(t *testing.T) {
+	// invalid height
+	cmd := exec.Command(CmdClient, "getblock", "--hash", BlockHash, "--address", ServerAddr)
+	if _, err := cmd.CombinedOutput(); err != nil {
+		t.Fatalf("Test_Client_GetBlock_ByNormalHash error, %s", err)
+	}
+}
+
+func Test_Client_GetBlock_ByInvalidHash(t *testing.T) {
+	// invalid height
+	cmd := exec.Command(CmdClient, "getblock", "--hash", BlockHashErr, "--address", ServerAddr)
+	if _, err := cmd.CombinedOutput(); err == nil {
+		t.Fatalf("Test_Client_GetBlock_ByInvalidHash error parameter success?")
+	}
+}
+
+// getblock fulltx support.
+func Test_Client_GetBlock_ByHeightFulltx(t *testing.T) {
+	cmd := exec.Command(CmdClient, "getblock", "--height", "1", "--fulltx", "--address", ServerAddr)
+	if output, err := cmd.CombinedOutput(); err != nil {
+		t.Fatalf("Test_Client_GetBlock_ByHeightFulltx error, %s", err)
+	} else {
+		var blockInfo BlockInfo
+		if err = json.Unmarshal(output, &blockInfo); err != nil {
+			t.Fatalf("Test_Client_GetBlock_ByHeightFulltx: %s", err)
+		}
+
+		headerMp := blockInfo.Header.(map[string]interface{})
+		height := uint64(headerMp["Height"].(float64))
+		if height != 1 {
+			t.Fatalf("Test_Client_GetBlock_ByHeightFulltx: Expect the return value is not correct!")
+		}
+		if len(blockInfo.Transactions) <= 0 {
+			t.Fatalf("Test_Client_GetBlock_ByHeightFulltx, block should contain one transaction at lease")
+		}
+	}
+}
+
+// getblock fulltx support.
+func Test_Client_GetBlock_ByHashFulltx(t *testing.T) {
+	cmd := exec.Command(CmdClient, "getblock", "--hash", BlockHash, "--fulltx", "--address", ServerAddr)
+	if output, err := cmd.CombinedOutput(); err != nil {
+		t.Fatalf("Test_Client_GetBlock_ByHashFulltx error, %s", err)
+	} else {
+		var blockInfo BlockInfo
+		if err = json.Unmarshal(output, &blockInfo); err != nil {
+			t.Fatalf("Test_Client_GetBlock_ByHashFulltx: %s", err)
+		}
+		if blockInfo.Hash != BlockHash {
+			t.Fatalf("Test_Client_GetBlock_ByHashFulltx: Expect the return value is not correct!")
+		}
+		if len(blockInfo.Transactions) <= 0 {
+			t.Fatalf("Test_Client_GetBlock_ByHashFulltx, hash should contain one transaction at lease")
+		}
+	}
+}
